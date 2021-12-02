81b7f7db6865399f6d151ca63b673ece7b597bd1
Study(ByoungChul) <skyksit@gmail.com>
2021-12-02 11:25:43 +0900

    UPDATE scraper function

--- pageScraper.js
+++ pageScraper.js
@@ -1,34 +1,42 @@
 const scraperObject = {
-  url: 'https://www.retrogames.cc/arcade-games/page/1.html',
-  async scraper(browser) {
+  url: "https://www.retrogames.cc/arcade-games/page/1.html",
+  async getGameList(browser) {
     let page = await browser.newPage();
     console.log(`Navigating to ${this.url}`);
     await page.goto(this.url);
+    // Get the games list scraped from the page
 
-    // Get the links to the games
-    console.log(`1`);
     await page.waitForSelector("div.post-des");
-    console.log(`2`);
-    let gameLinks = await page.evaluate(() => {
-      let gameArray = [];
-      let gameTitles;
-
-      gameTitles = document.querySelectorAll("div.post-des h6 a");
-
-      for (let i = 0; i < gameTitles.length; i++) {
-        console.log(i);
-        // let game = {
-        //   title: gameTitles[i].innerText,
-        // }
-        console.log(gameTitles[i].innerText);
-        gameArray.push(gameTitles[i].innerText);
-      }
-
-      return gameArray;
+    let gameList = await page.evaluate(() => {
+      let gameList = [];
+      let gameListItems = document.querySelectorAll("div.post-des h6 a");
+      gameListItems.forEach((game) => {
+        gameList.push({
+          title: game.innerText,
+          url: game.href,
+        });
+      });
+      return gameList;
     });
-    console.log(`Found ${gameLinks.length} games`);
-    console.log(gameLinks);
-  }
-}
+    console.log(`Found ${gameList.length} games`);
+    return gameList;
+  },
+  async getGameDetails(browser, url) {
+    let newPage = await browser.newPage();
+    console.log(`Navigating to ${url}`);
+    await newPage.goto(url);
+    // Get the game details scraped from the page
+    await newPage.waitForSelector("textarea");
+    let gameDetails = await newPage.evaluate(() => {
+      let gameDetails = document.querySelector("textarea");
+      //get textarea value
+      console.log(`gameDetails.value = ${gameDetails.value}`);
+      let gameDetailsText = gameDetails.value;
+      return gameDetailsText;
+    });
+    await newPage.close();
+    return gameDetails;
+  },
+};
 
 module.exports = scraperObject;
--- scraperController.js
+++ scraperController.js
@@ -1,9 +1,19 @@
 const pageScraper = require('./pageScraper');
 async function scrapeAll(browserInstance) {
   let browser;
+  let gameArray;
   try {
     browser = await browserInstance;
-    await pageScraper.scraper(browser);
+    //get GameList await
+    gameArray = await pageScraper.getGameList(browser);
+    //get GameDetails await
+    for (let i = 0; i < gameArray.length; i++) {
+      let gameDetails = await pageScraper.getGameDetails(
+        browser,
+        gameArray[i].url
+      );
+      console.log(gameDetails);
+    }
   } catch (error) {
     console.log("Could not resolve the browser instance => ", error);
   }
